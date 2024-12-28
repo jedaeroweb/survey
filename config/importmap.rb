c8dd66a87242539bc04708b4f7f8a10bf3b51ac1
sleepinglion <admin@sleepinglion.pe.kr>
2024-12-28 16:41:26 +0900

    수정

---
+++ config/importmap.rb
@@ -0,0 +1,9 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "rails-ujs", to: "rails-ujs.js"
+pin "jquery" # @3.7.1
+pin "bootstrap", to: "bootstrap.min.js"
+pin "common", to: "common.js"
+pin "index", to: "index.js"
